6d50b4c2757e24d0fb0a962e46e7f233bb44dee7
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2023-12-31 15:19:03 +0800

    update NymuStatic/pengaturan.js

--- NymuStatic/pengaturan.js
+++ NymuStatic/pengaturan.js
@@ -1,5 +1,6 @@
 // Brought to you by Anonym2Name
 import {KepalaHTML, TubuhHTML} from './elemen.js';
+import {eruda} from './Eruda/eruda.js';
 // endof !import
 
 // Impor file CSS sekalian masukin tag <meta>

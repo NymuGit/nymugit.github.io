003b3e7eaea552897588c3fd9d46fc3d1a78eeec
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2023-12-31 14:41:39 +0800

    update NymuStatic/pengaturan.js

--- NymuStatic/pengaturan.js
+++ NymuStatic/pengaturan.js
@@ -4,3 +4,6 @@
 
 let KepalaHTML = document.getElementsByTagName('head')[0];
 let TubuhHTML = document.getElementsByTagName('body')[0];
+
+// Impor file CSS sekalian masukin tag <meta>
+KepalaHTML.write = '<meta name="viewport" content="width=device-width, initial-scale=1">';

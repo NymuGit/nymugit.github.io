586ce3b2d66ba6bd0f9c1da13738a4c6542d824c
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2023-12-31 14:50:15 +0800

    update NymuStatic/elemen.js

--- NymuStatic/elemen.js
+++ NymuStatic/elemen.js
@@ -2,4 +2,4 @@ let KepalaHTML = document.getElementsByTagName('head')[0];
 let TubuhHTML = document.getElementsByTagName('body')[0];
 
 // Jangan lupa ekspor!
-export
+export {KepalaHTML, TubuhHTML};

823bfa93773669ec4a55d3930d284e975e7335f4
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-01 17:43:04 +0800

    update NymuStatic/pengaturan.js

--- NymuStatic/pengaturan.js
+++ NymuStatic/pengaturan.js
@@ -1,8 +1,8 @@
 // Brought to you by Anonym2Name
-import * from './elemen.js';
+import * as Elmn from './elemen.js';
 import * as Fx from './fungsi.js';
 // endof !import
 
 // Jalanin fungsi yang sudah diekspor
-UrlData, Bahasa, NymuApp;
+Elmn.UrlData, Elmn.Bahasa, Elmn.NymuApp;
 Fx.DeteksiUrl(UrlData, Bahasa);

6f693791b052bfd517c6765eadac0cd487752519
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-01 13:29:34 +0800

    update NymuStatic/elemen.js

--- NymuStatic/elemen.js
+++ NymuStatic/elemen.js
@@ -1,7 +1,7 @@
 // Brought to you by Anonym2Name
 let KepalaHTML = document.getElementsByTagName('head')[0];
 let TubuhHTML = document.getElementsByTagName('body')[0];
-let UrlData = location.pathname;
+let UrlData = window.location.pathname;
 let Debugger = '<script>eruda.init();</script>';
 
 // Jangan lupa ekspor!

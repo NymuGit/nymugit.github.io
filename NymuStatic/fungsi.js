f8cf0ff78f51a2e996e1afcb94ea950be93c744a
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-01 17:17:06 +0800

    update NymuStatic/fungsi.js

--- NymuStatic/fungsi.js
+++ NymuStatic/fungsi.js
@@ -1,5 +1,5 @@
 // Brought to you by Anonym2Name
 export var DeteksiUrl = (name, language, uniqueID) => {
   if (name == '/beranda.htm?id' && language == 'id') console.log('Bahasa Indo!');
-  else if (name == '/beranda.htm') confirm('Choose a language \n    Pilih bahasa');
+  
 };

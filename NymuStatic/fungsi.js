bafeecc40207083cd9ded8bda0e9df27f4747bfe
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-02 13:31:52 +0800

    update NymuStatic/fungsi.js

--- NymuStatic/fungsi.js
+++ NymuStatic/fungsi.js
@@ -1,4 +1,4 @@
 // Brought to you by Anonym2Name
 export let DeteksiUrl = (name, language, uniqueID) => {
-  if (name == '/beranda.htm' && language == 'en') window.confirm('what you want?');
+  if (name == '/beranda.htm' && language == 'en-US') window.confirm('what you want?');
 };

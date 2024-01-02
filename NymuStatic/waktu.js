aa74769ab2741416c3311b28b34289db5207d719
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-02 18:17:06 +0800

    update NymuStatic/waktu.js

--- NymuStatic/waktu.js
+++ NymuStatic/waktu.js
@@ -1,1 +1,11 @@
 // Brought to you by Anonym2Name
+let Kalendar = new Date();
+let Jam = Kalendar.getHours();
+let Menit = Kalendar.getMinutes();
+let Detik = Kalendar.getSeconds();
+
+// Ekspor semua!
+export{
+  Kalendar, 
+  Jam, Menit, Detik
+};

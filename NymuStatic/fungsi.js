607c0a216e5b0a07af717b6abe36c6919eab489c
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-02 12:32:24 +0800

    update NymuStatic/fungsi.js

--- NymuStatic/fungsi.js
+++ NymuStatic/fungsi.js
@@ -1,11 +1,11 @@
 // Brought to you by Anonym2Name
 export var DeteksiUrl = (name, language, uniqueID) => {
   if (language == 'en' && name == '/beranda.htm' || language == 'en') {
-    alert('English!');
+    console.log('English!');
     location.assign('/beranda.htm?en');
   }
   if (language == 'id' && name == '/beranda.htm' || language == 'id') {
-    alert('Indonesia!');
+    console.log('Indonesia!');
     location.assign('/beranda.htm?id');
   }
 };

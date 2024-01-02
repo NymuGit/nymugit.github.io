fc789d097018d7b3b5dc3ceb2556995745d1406b
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-02 13:24:43 +0800

    update NymuStatic/fungsi.js

--- NymuStatic/fungsi.js
+++ NymuStatic/fungsi.js
@@ -1,7 +1,4 @@
 // Brought to you by Anonym2Name
-export var DeteksiUrl = (name, language) => {
-  if (name == '/beranda.htm' && language == 'en-US') window.location.replace('/beranda.htm?en');
-};
-export var TampilanHTML = (name, uniqueID) => {
-  if (name == '/beranda.htm?en') uniqueID.innerHTML = '<p>' + 'Hello guys!' + '</p>';
+export let DeteksiUrl = () => {
+  if (name == '/beranda.htm' && language == 'en') uniqueID.alert('');
 };

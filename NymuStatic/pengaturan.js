d8cc204ba4b1f9c5165c53c39d9f4cb838815945
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-03 10:29:50 +0800

    update NymuStatic/pengaturan.js

--- NymuStatic/pengaturan.js
+++ NymuStatic/pengaturan.js
@@ -10,7 +10,7 @@ import * as Fx from './fungsi.js';
 // endof !import
 
 // Jalanin fungsi yang sudah diekspor
-KepalaHTML, TubuhHTML;
+HTML, KepalaHTML, TubuhHTML;
 UrlData, Bahasa, NymuApp;
 Kalendar, Jam, Menit, Detik;
 Fx.DeteksiUrl(UrlData, Bahasa, NymuApp);

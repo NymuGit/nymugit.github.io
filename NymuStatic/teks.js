431b9119d7db2ba7e3331460222663eabf45eaf3
Aku2Nama <145840403+NymuGit@users.noreply.github.com>
2024-01-09 08:15:32 +0800

    update NymuStatic/teks.js

--- NymuStatic/teks.js
+++ NymuStatic/teks.js
@@ -1,5 +1,9 @@
 // Brought to you by Anonym2Name
 let Nymu_Icon_alt = `Nymu icon`;
+let Footer_content = `
+&copy; 2020-2024 Anonym Nicknamers. 
+Hosted by Github.
+`
 
 // Jangan lupa ekspor!
 export {
